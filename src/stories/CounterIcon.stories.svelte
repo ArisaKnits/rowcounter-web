4117624f1fb41f5c68c57cea6d5815d788383b5e
Burton Johnsey <git@tonjohn.com>
2026-09-23 12:48:12 -0700

    feat: CounterIcon component

---
+++ src/stories/CounterIcon.stories.svelte
@@ -0,0 +1,37 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import CounterIcon from './CounterIcon.svelte';
+	import { fn } from 'storybook/test';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/CounterIcon',
+		component: CounterIcon,
+		globals: {
+			backgrounds: { value: 'dark'}
+		},
+		tags: ['autodocs'],
+		argTypes: {
+			icon: {
+				control: { type: 'select' },
+				options: ['hankncrochet', 'hanknknit', 'cake', 'mittens', 'socks', 'sweater']
+			}
+		},
+		args: {
+			onclick: fn()
+		}
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Hank'n'Crochet" args={{ icon: 'hankncrochet' }} />
+
+<Story name="Hank'n'Knit" args={{ icon: 'hanknknit' }} />
+
+<Story name="Cake" args={{ icon: 'cake' }} />
+
+<Story name="Mittens" args={{ icon: 'mittens' }} />
+
+<Story name="Socks" args={{ icon: 'socks' }} />
+
+<Story name="Sweater" args={{ icon: 'sweater' }} />
---
+++ src/stories/CounterIcon.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import hankncrochet from './assets/counter-icons/hankncrochet.png';
+	import hanknknit from './assets/counter-icons/hanknknit.png';
+	import cake from './assets/counter-icons/cake.png';
+	import mittens from './assets/counter-icons/mittens.png';
+	import socks from './assets/counter-icons/socks.png';
+	import sweater from './assets/counter-icons/sweater.png';
+
+	interface Props {
+		/** Which counter icon to display */
+		icon: 'hankncrochet' | 'hanknknit' | 'cake' | 'mittens' | 'socks' | 'sweater';
+		/** The onclick event handler */
+		onclick?: () => void;
+	}
+
+	const { icon, onclick, ...props }: Props = $props();
+
+	let iconInfo = $derived.by(() => {
+		switch (icon) {
+			case 'hankncrochet':
+				return { src: hankncrochet, alt: 'Hank n Crochet' };
+			case 'hanknknit':
+				return { src: hanknknit, alt: 'Hank n Knit' };
+			case 'cake':
+				return { src: cake, alt: 'Cake' };
+			case 'mittens':
+				return { src: mittens, alt: 'Mittens' };
+			case 'socks':
+				return { src: socks, alt: 'Socks' };
+			case 'sweater':
+				return { src: sweater, alt: 'Sweater' };
+		}
+	});
+</script>
+
+<img
+	src={iconInfo.src}
+	alt={iconInfo.alt}
+	class={['storybook-button', `storybook-button--${icon}`].join(' ')}
+	{onclick}
+	{...props}
+/>
